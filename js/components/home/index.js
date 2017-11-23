5328b4e21086dae4e8e67ed5f2453af1f9ce4b21
Aykut DEMİREL <ayktdemirel@hotmail.com>
2017-11-23 17:52:05 +0300

    updated

--- js/components/home/index.js
+++ js/components/home/index.js
@@ -1,137 +1,211 @@
-import React, { Component } from "react";
-import { TouchableOpacity, Image } from "react-native";
-import { connect } from "react-redux";
+import React, {Component} from "react";
+import {TouchableOpacity, Image, PanResponder, Animated} from "react-native";
+import {connect} from "react-redux";
 import BlankPage2 from "../blankPage2";
 import DrawBar from "../DrawBar";
 
-import { DrawerNavigator, NavigationActions } from "react-navigation";
+import {DrawerNavigator, NavigationActions} from "react-navigation";
 
 const norwadian = require("../../../images/norwegiangirl15.jpg");
 
 import {
-  Container,
-  Header,
-  Title,
-  Content,
-  Text,
-  Button,
-  Icon,
-  Left,
-  Body,
-  View,
-  Right
+    Container,
+    Header,
+    Title,
+    Content,
+    Text,
+    Button,
+    Icon,
+    Left,
+    Body,
+    View,
+    Right
 } from "native-base";
 
-import { Grid, Row } from "react-native-easy-grid";
+import {Grid, Row} from "react-native-easy-grid";
 
-import { setIndex } from "../../actions/list";
-import { openDrawer } from "../../actions/drawer";
+import {setIndex} from "../../actions/list";
+import {openDrawer} from "../../actions/drawer";
 import styles from "./styles";
 
 class Home extends Component {
-  static navigationOptions = {
-    header: null
-  };
-  static propTypes = {
-    name: React.PropTypes.string,
-    setIndex: React.PropTypes.func,
-    list: React.PropTypes.arrayOf(React.PropTypes.string),
-    openDrawer: React.PropTypes.func
-  };
 
-  newPage(index) {
-    this.props.setIndex(index);
-    Actions.blankPage();
-  }
+    static navigationOptions = {
+        header: null
+    };
 
-  render() {
-    console.log(DrawNav, "786785786");
-    return (
-      <Container style={styles.container}>
-        <Header>
-          <Left>
+    static propTypes = {
+        name: React.PropTypes.string,
+        setIndex: React.PropTypes.func,
+        list: React.PropTypes.arrayOf(React.PropTypes.string),
+        openDrawer: React.PropTypes.func
+    };
 
-            <Button
-              transparent
-              onPress={() => {
-                DrawerNav.dispatch(
-                  NavigationActions.reset({
-                    index: 0,
-                    actions: [NavigationActions.navigate({ routeName: "Home" })]
-                  })
-                );
-                DrawerNav.goBack();
-              }}
-            >
-              <Icon active name="power" />
-            </Button>
-          </Left>
+    newPage(index) {
+        this.props.setIndex(index);
+        Actions.blankPage();
+    }
 
-          <Body>
-            <Title>Main Page</Title>
-          </Body>
+    constructor(props) {
 
-          <Right>
-            <Button
-              transparent
-              onPress={() => DrawerNav.navigate("DrawerOpen")}
-            >
-              <Icon active name="menu" />
-            </Button>
-          </Right>
-        </Header>
-        <Content>
-          <Image source={norwadian} style={styles.shadow}>
-            <View style={styles.bg}>
-            </View>
-          </Image>
-          <Grid style={styles.mt}>
-            {this.props.list.map((item, i) => (
-              <Row key={i}>
-                <TouchableOpacity
-                  style={styles.row}
-                  onPress={() =>
-                    this.props.navigation.navigate("BlankPage", {
-                      name: { item }
-                    })}
-                >
-                  <Text style={styles.text}>{item}</Text>
-                </TouchableOpacity>
-              </Row>
-            ))}
-          </Grid>
-        </Content>
-      </Container>
-    );
-  }
+        super(props);
+
+        this.state = {
+            // create an instance of Animated.ValueXY, whcih will take care of interpolating X and Y values
+            pan: new Animated.ValueXY(),
+            showDraggable: true,
+            dropZoneValues: null
+        };
+        // PanResponder, which is responsible for doing the dragging, sets the handlers when the user moves and releases the element
+        // Animated.spring method runs the animation
+        // first parameter (i.e. this.state.pan) accepts the animation values
+        // second parameter (i.e. {toValue:{x:0,y:0}}) is a configuration object that defines the toValue, which is the origin coordinates
+        this.panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: Animated.event([null, { //the handler will trigger when the element is moving
+                dx: this.state.pan.x,
+                dy: this.state.pan.y
+            }]),
+            onPanResponderRelease: (e, gesture) => {
+                if (this.isDropZone(gesture)) { //Step 1
+                    this.setState({
+                        showDraggable: false //Step 3
+                    });
+                } else {
+                    Animated.spring(
+                        this.state.pan,
+                        {toValue: {x: 0, y: 0}}
+                    ).start();
+                }
+            }
+        });
+    }
+
+    isDropZone(gesture) {
+        var dz = this.state.dropZoneValues;
+        return gesture.moveY > dz.y && gesture.moveY < dz.y + dz.height;
+    }
+
+    setDropZoneValues(event) {
+        this.setState({
+            dropZoneValues: event.nativeEvent.layout
+        });
+    }
+
+    renderDraggable() {
+        // renders green circle based on showDraggable value
+        if (this.state.showDraggable) {
+            return (
+                <View style={styles.draggableContainer}>
+                    <Animated.View
+                        {...this.panResponder.panHandlers}
+                        style={[this.state.pan.getLayout()]}>
+                        <Image source={norwadian} style={styles.shadow}>
+                            <View style={styles.bg}>
+                            </View>
+                        </Image>
+                    </Animated.View>
+                </View>
+            );
+        }
+    }
+
+    render() {
+        console.log(DrawNav, "786785786");
+        return (
+            <Container style={styles.container}>
+                <Header>
+                    <Left>
+
+                        <Button
+                            transparent
+                            onPress={() => {
+                                DrawerNav.dispatch(
+                                    NavigationActions.reset({
+                                        index: 0,
+                                        actions: [NavigationActions.navigate({routeName: "Home"})]
+                                    })
+                                );
+                                DrawerNav.goBack();
+                            }}
+                        >
+                            <Icon active name="power"/>
+                        </Button>
+                    </Left>
+
+                    <Body>
+                    <Title>Main Page</Title>
+                    </Body>
+
+                    <Right>
+                        <Button
+                            transparent
+                            onPress={() => DrawerNav.navigate("DrawerOpen")}
+                        >
+                            <Icon active name="menu"/>
+                        </Button>
+                    </Right>
+                </Header>
+                <Content>
+
+                    <View style={styles.mainContainer}>
+                        <View
+                            onLayout={this.setDropZoneValues.bind(this)}
+                            style={styles.dropZone}>
+                            <Text style={styles.topText}>Drop me here!</Text>
+                        </View>
+
+                        {this.renderDraggable()}
+                    </View>
+
+                    {/*<Grid style={styles.mt}>*/}
+                        {/*{this.props.list.map((item, i) => (*/}
+                            {/*<Row key={i}>*/}
+                                {/*<TouchableOpacity*/}
+                                    {/*style={styles.row}*/}
+                                    {/*onPress={() =>*/}
+                                        {/*this.props.navigation.navigate("BlankPage", {*/}
+                                            {/*name: {item}*/}
+                                        {/*})}*/}
+                                {/*>*/}
+                                    {/*<Text style={styles.text}>{item}</Text>*/}
+                                {/*</TouchableOpacity>*/}
+                            {/*</Row>*/}
+                        {/*))}*/}
+                    {/*</Grid>*/}
+                </Content>
+            </Container>
+        );
+    }
 }
 
 function bindAction(dispatch) {
-  return {
-    setIndex: index => dispatch(setIndex(index)),
-    openDrawer: () => dispatch(openDrawer())
-  };
+    return {
+        setIndex: index => dispatch(setIndex(index)),
+        openDrawer: () => dispatch(openDrawer())
+    };
 }
+
 const mapStateToProps = state => ({
-  name: state.user.name,
-  list: state.list.list
+    name: state.user.name,
+    list: state.list.list
 });
 
 const HomeSwagger = connect(mapStateToProps, bindAction)(Home);
 const DrawNav = DrawerNavigator(
-  {
-    Home: { screen: HomeSwagger },
-    BlankPage2: { screen: BlankPage2 }
-  },
-  {
-    contentComponent: props => <DrawBar {...props} />
-  }
+    {
+        Home: {screen: HomeSwagger},
+        BlankPage2: {screen: BlankPage2}
+    },
+    {
+        contentComponent: props => <DrawBar {...props} />
+    }
 );
 const DrawerNav = null;
-DrawNav.navigationOptions = ({ navigation }) => {
-  DrawerNav = navigation;
-  return {
-    header: null
-  };
+DrawNav.navigationOptions = ({navigation}) => {
+    DrawerNav = navigation;
+    return {
+        header: null
+    };
 };
 export default DrawNav;
--- js/components/home/styles.js
+++ js/components/home/styles.js
@@ -1,38 +1,59 @@
-
 const React = require('react-native');
 
-const { StyleSheet, Dimensions } = React;
+const {StyleSheet, Dimensions} = React;
 
-const deviceHeight = Dimensions.get('window').height;
+const device_size = Dimensions.get('window');
+
+const CIRCLE_RADIUS = 36;
 
 export default {
-  container: {
-    backgroundColor: '#FBFAFA',
-  },
-  shadow: {
-      flex: 1,
-      width: null,
-      height: null,
-  },
-  bg: {
-      flex: 1,
-      marginTop: deviceHeight / 1.75,
-      paddingTop: 20,
-      paddingLeft: 10,
-      paddingRight: 10,
-      paddingBottom: 30,
-      bottom: 0,
-  },
-  row: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 20,
-    marginBottom: 15,
-    alignItems: 'center',
-  },
-  mt: {
-    marginTop: 18,
-  },
+    container: {
+        backgroundColor: '#FBFAFA',
+    },
+    shadow: {
+        flex: 1,
+        width: null,
+        height: null,
+    },
+    bg: {
+        flex: 1,
+        marginTop: device_size.height / 1.75,
+        paddingTop: 20,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 30,
+        bottom: 0,
+    },
+    row: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 20,
+        marginBottom: 15,
+        alignItems: 'center',
+    },
+    mt: {
+        marginTop: 18,
+    },
+    mainContainer: {
+        flex: 1
+    },
+    dropZone: {
+        height: 70,
+        backgroundColor: '#2c3e50'
+    },
+    topText: {
+        marginTop: 25,
+        marginLeft: 5,
+        marginRight: 5,
+        textAlign: 'center',
+        color: '#fff'
+    },
+    draggableContainer: {
+        position: 'absolute',
+        top: 70,
+        left: 0,
+        width: device_size.width
+    }
 };
